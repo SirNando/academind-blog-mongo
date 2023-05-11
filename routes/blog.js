4ab8bf7135fa3bcc51e769e402a3fa24b8f63b79
SirNando <matiasnandin@hotmail.com>
2023-05-11 18:04:36 -0300

    Updated files

--- routes/blog.js
+++ routes/blog.js
@@ -12,8 +12,13 @@ router.get('/', function(req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get('/posts', async function(req, res) {
+  const posts = await db
+  .getDb()
+  .collection('posts')
+  .find({}, {title: 1, summary: 1, 'author.name': 1})
+  .toArray();
+  res.render('posts-list', {posts: posts});
 });
 
 router.post('/posts', async function(req, res) {
@@ -37,10 +42,47 @@ router.post('/posts', async function(req, res) {
   res.redirect('/posts');
 });
 
+router.get('/post/:id', async function(req, res) {
+  const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(req.params.id)}, {title: 1, content: 1, 'author.name': 1});
+  if(!post) {
+    return res.status(404).render('404');
+  }
+  res.render('includes/post-item', {post: post});
+});
+
+router.get('/update-post/:id', async function(req, res) {
+  const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(req.params.id)}, {date: 0, author: 0});
+  if(!post) {
+    res.status(404).render('404');
+  }
+  res.render('update-post', {post: post});
+});
+
+router.post('/update-post/:id', async function(req, res) {
+  const updates = {
+    title: req.body.title,
+    summary: req.body.summary,
+    content: req.body.content
+  };
+  const post = await db.getDb().collection('posts').updateOne(
+    {_id: new ObjectId(req.params.id)},
+    {
+      $set: {
+      title: updates.title,
+      summary: updates.summary,
+      content: updates.content
+    }});
+  res.redirect('/posts');
+});
+
 router.get('/new-post', async function(req, res) {
   const authors = await db.getDb().collection('authors').find().toArray();
-  console.log(authors);
   res.render('create-post', {authors: authors});
 });
+
+router.post('/delete-post/:id', async function(req, res) {
+  await db.getDb().collection('posts').deleteOne({_id: new ObjectId(req.params.id)});
+  res.redirect('/posts');
+})
 
 module.exports = router;
