b048327014f4cb9e5a0afad3efbf50fe27262887
SirNando <matiasnandin@hotmail.com>
2023-05-15 08:01:49 -0300

    Added error handling middleware

--- routes/blog.js
+++ routes/blog.js
@@ -42,8 +42,14 @@ router.post('/posts', async function(req, res) {
   res.redirect('/posts');
 });
 
-router.get('/post/:id', async function(req, res) {
-  const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(req.params.id)}, {title: 1, content: 1, 'author.name': 1});
+router.get('/post/:id', async function(req, res, next) {
+
+  try {
+    const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(req.params.id)}, {title: 1, content: 1, 'author.name': 1});
+  } catch (error) {
+    next(error);
+  }
+  
   if(!post) {
     return res.status(404).render('404');
   }
